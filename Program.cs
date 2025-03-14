013c068af2a1ad93b4e2b8d8c331182bb06d7aaf
Cayden Andrews <144056074+D761454@users.noreply.github.com>
2025-03-14 17:19:30 +0000

    add code

---
+++ Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+class Program
+{
+    static void Main()
+    {
+        string filename = "wordlist.txt";
+        int anagrams = 0;
+
+        try
+        {
+            List<string> lines = new List<string>();
+            Dictionary<int, List<string>> wordsByLength = new Dictionary<int, List<string>>();
+            List<string> searched = new List<string>();
+
+            using (StreamReader sr = new StreamReader(filename))
+            {
+                string line;
+                while((line = sr.ReadLine()) != null)
+                {
+                    lines.Add(line);
+                }
+            }
+
+            // sort into length
+            //lines = lines.OrderBy(x => x.Length).ToList();
+
+            // sort all into alphabetical order
+            for (int i = 0; i < lines.Count; i++)
+            {
+                lines[i] = lines[i].ToLower().Trim();
+                char[] chars = lines[i].ToArray();
+                Array.Sort(chars);
+                lines[i] = new string(chars);
+            }
+
+            var res = lines.GroupBy(x => x);
+
+            foreach (var e in res)
+            {
+                if (e.Count() > 1)
+                {
+                    anagrams++;
+                }
+            }
+
+            Console.WriteLine(anagrams);
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("Error: " + ex.Message);
+        }
+    }
+}
+
+struct Pair
+{
+    public string first { get; private set; }
+    public string second { get; private set; }
+
+    public Pair(string first, string second)
+    {
+        this.first = first;
+        this.second = second;
+    }
+}
